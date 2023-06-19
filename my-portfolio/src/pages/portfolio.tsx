f1e814ded4263be370e658e0859a312caeb3e57a
Rahsan Lewis <rahsanlewis@gmail.com>
2023-06-19 17:13:03 -0400

    fixed apos error

--- my-portfolio/src/pages/portfolio.tsx
+++ my-portfolio/src/pages/portfolio.tsx
@@ -15,7 +15,7 @@ const ProjectsPage = () => {
     return (
         <Layout>
             <div className="flex flex-col items-center">
-                <h2 className="text-2xl m-6 p-4 text-bold text-black">Here is where you'll find all my projects!</h2>
+                <h2 className="text-2xl m-6 p-4 text-bold text-black">Here is where you will find all my projects!</h2>
                 {projects.map((project, index) => (
                     <Project key={index} project={project} />
                 ))}
