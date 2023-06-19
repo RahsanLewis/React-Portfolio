c840f67fd2b4bbc92a58cd3f11136c18e3ed4c81
Rahsan Lewis <rahsanlewis@gmail.com>
2023-06-19 17:15:46 -0400

    fixed more apos errors

--- my-portfolio/src/pages/about.tsx
+++ my-portfolio/src/pages/about.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link';
 const AboutPage = () => (
     <Layout>
         <div className="flex flex-col items-center mt-6 p-6">
-            <p className="text-xl text-black text-center m-4 p-2">Hello! I'm a full-stack developer based in Central Pennsylvania with a specialty in building applications using React. I have a strong passion for coding and love every aspect of my work.
-                For years I've been constantly striving to improve my skills and learn new technologies. The joy I find in solving complex problems and creating intuitive, dynamic user experiences is what drives me in this rapidly evolving industry.</p>
+            <p className="text-xl text-black text-center m-4 p-2">Hello! I am a full-stack developer based in Central Pennsylvania with a specialty in building applications using React. I have a strong passion for coding and love every aspect of my work.
+                For years I have been constantly striving to improve my skills and learn new technologies. The joy I find in solving complex problems and creating intuitive, dynamic user experiences is what drives me in this rapidly evolving industry.</p>
 
-            <p className="text-xl text-black text-center m-4 p-2">When I'm not in front of a computer screen, I enjoy playing basketball, traveling, and spending time with family and friends.
+            <p className="text-xl text-black text-center m-4 p-2">When I am not in front of a computer screen, I enjoy playing basketball, traveling, and spending time with family and friends.
                 I'm always up for a new challenge and look forward to bringing my passion to new projects.</p>
 
             <p className="text-xl text-black text-center m-4 p-2">
-                If you'd like to know more about my work or discuss potential opportunities, please feel free to get in
+                If you would like to know more about my work or discuss potential opportunities, please feel free to get in
                 <Link href="/contact">
                     <span className="text-blue-500 cursor-pointer hover:text-blue-300 transition-colors duration-200"> touch</span>
                 </Link>
